2501a71511da090ba34c984c5b6c4b1ee795511a
leisuredong <370324425@qq.com>
2019-12-03 14:53:55 +0800

    Add RestTemplate POST to invoke method on another module

--- demo-gateway/src/main/java/com/example/demo/gateway/DemoGatewayApplication.java
+++ demo-gateway/src/main/java/com/example/demo/gateway/DemoGatewayApplication.java
@@ -3,11 +3,14 @@ package com.example.demo.gateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -25,5 +28,11 @@ public class DemoGatewayApplication {
     @RequestMapping("/hi")
     public String hello(@RequestParam(value = "name", defaultValue = "demo-gateway") String name) {
         return "Hi " + name + ", I am from port:" + port;
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate initRestTemplate() {
+        return new RestTemplate();
     }
 }
